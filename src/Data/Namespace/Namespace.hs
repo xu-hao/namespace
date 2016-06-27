e338ccfe8d76ce8addfe1e32f7a72bbaaa744917
Hao Xu <xuh@email.unc.edu>
2016-06-27 01:34:57 -0400

    add insert object

--- src/Data/Namespace/Namespace.hs
+++ src/Data/Namespace/Namespace.hs
@@ -1,12 +1,13 @@
 {-# LANGUAGE RankNTypes, GADTs, FlexibleInstances #-}
 
 module Data.Namespace.Namespace
-    ( Namespace, lookupNamespace, lookupObject, namespace, topLevelObjects
+    ( Namespace, lookupNamespace, lookupObject, namespace, topLevelObjects, insertObject
     ) where
 
 import Prelude hiding (lookup)
 import Data.Map.Strict
 import Data.Monoid
+import Data.Maybe
 
 import Data.Namespace.Path
 
@@ -29,3 +30,10 @@ namespace = Namespace mempty mempty
 
 topLevelObjects :: Key k => Namespace k a -> Map k a
 topLevelObjects (Namespace _ om) = om
+
+insertObject :: Key k => ObjectPath k -> a -> Namespace k a -> Namespace k a
+insertObject (ObjectPath (NamespacePath []) k) o (Namespace nm om) = Namespace nm (insert k o om)
+insertObject (ObjectPath (NamespacePath (k : ks)) objkey) o (Namespace nm om) =
+  let nm2 = fromMaybe namespace (lookup k nm)
+      nm2' = insertObject (ObjectPath (NamespacePath ks) objkey) o nm2 in
+      Namespace (insert k nm2' nm) om
